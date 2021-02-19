3a02d1f3a6542dd3eeafb80abe4290f6b5047713
ndabe <stonerlife420k@gmail.com>
2021-02-19 11:29:31 +0000

    Alien project

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+	"""A class to store all settings for Alien Invasion."""
+	def __init__(self):
+		"""Initialize the game's settings."""
+		# Screen settings
+		self.screen_width = 1000
+		self.screen_height = 500
+		self.bg_color = (37, 54, 148)
+		self.ship_speed = 0.75
